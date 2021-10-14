3b99624b534791cc29a1f88c0956884c0a093bcc
ovaphlow <ovaphlow@live.com>
2021-10-14 17:15:06 +0800

    首页统计

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -10,17 +10,6 @@ import BottomNav from '../component/BottomNav';
 import useAuth from '../useAuth';
 import { reducer } from '../miscellaneous';
 
-const initial_state = {
-  candidate_today: 0,
-  candidate_all: 0,
-  candidate_send: 0,
-  employer_today: 0,
-  employer_all: 0,
-  employer_job: 0,
-  bulletin_tuijian_today: 0,
-  bulletin_tuijian_all: 0,
-};
-
 ReactDOM.render(
   <React.StrictMode>
     <Index />
@@ -42,100 +31,115 @@ function Index() {
 
 function Home() {
   const auth = useAuth();
-  const [state, dispatch] = React.useReducer(reducer, initial_state);
+  const [state, dispatch] = React.useReducer(reducer, {
+    candidate_today: 0,
+    candidate_all: 0,
+    candidate_send_all: 0,
+    candidate_send_today: 0,
+    employer_today: 0,
+    employer_all: 0,
+    employer_job_all: 0,
+    employer_job_today: 0,
+    bulletin_tuijian_today: 0,
+    bulletin_tuijian_all: 0,
+    bulletin_campus_all: 0,
+    bulletin_campus_today: 0,
+  });
 
   React.useEffect(() => {
     const auth = sessionStorage.getItem('mis-auth');
     if (!auth) {
-      window.location = SIGN_IN_URL;
+      location.href = SIGN_IN_URL;
       return;
     }
   });
 
   React.useEffect(() => {
-    fetch('/api/biz/candidate/statistic?option=today')
+    fetch('/api/biz/candidate/statistic?option=qty-by-total-today')
       .then((response) => response.json())
       .then((data) => {
         dispatch({
           type: 'set',
-          payload: { key: 'candidate_today', value: data.qty },
+          payload: { key: 'candidate_all', value: data.total + 200000 },
+        });
+        dispatch({
+          type: 'set',
+          payload: { key: 'candidate_today', value: data.today },
         });
       });
   }, []);
 
   React.useEffect(() => {
-    fetch('/api/biz/candidate/statistic?option=all')
+    fetch('/api/biz/employer/statistic?option=qty-by-total-today')
       .then((response) => response.json())
       .then((data) => {
         dispatch({
           type: 'set',
-          payload: { key: 'candidate_all', value: data.qty + 200000 },
+          payload: { key: 'employer_today', value: data.today },
+        });
+        dispatch({
+          type: 'set',
+          payload: { key: 'employer_all', value: data.total + 60000 },
         });
       });
   }, []);
 
   React.useEffect(() => {
-    fetch('/api/biz/employer/statistic?option=today')
+    fetch('/api/biz/send-in/statistic?option=qty-by-total-today')
       .then((response) => response.json())
       .then((data) => {
         dispatch({
           type: 'set',
-          payload: { key: 'employer_today', value: data.qty },
+          payload: { key: 'candidate_send_all', value: data.total + 180018 },
+        });
+        dispatch({
+          type: 'set',
+          payload: { key: 'candidate_send_today', value: data.today },
         });
       });
   }, []);
 
   React.useEffect(() => {
-    fetch('/api/biz/employer/statistic?option=all')
+    fetch('/api/biz/job/statistic?option=qty-by-total-today')
       .then((response) => response.json())
       .then((data) => {
         dispatch({
           type: 'set',
-          payload: { key: 'employer_all', value: data.qty + 60000 },
+          payload: { key: 'employer_job_all', value: data.total },
+        });
+        dispatch({
+          type: 'set',
+          payload: { key: 'employer_job_today', value: data.today },
         });
       });
   }, []);
 
   React.useEffect(() => {
-    fetch('/api/biz/send-in/statistic?option=all')
+    fetch('/api/bulletin/statistic?option=notification-qty-by-total-today')
       .then((response) => response.json())
       .then((data) => {
         dispatch({
           type: 'set',
-          payload: { key: 'candidate_send', value: data.qty + 180018 },
+          payload: { key: 'bulletin_tuijian_all', value: data.total },
+        });
+        dispatch({
+          type: 'set',
+          payload: { key: 'bulletin_tuijian_today', value: data.today },
         });
       });
   }, []);
 
   React.useEffect(() => {
-    fetch('/api/biz/job/statistic?option=all')
+    fetch('/api/bulletin/statistic?option=campus-qty-by-total-today')
       .then((response) => response.json())
       .then((data) => {
         dispatch({
           type: 'set',
-          payload: { key: 'employer_job', value: data.qty },
+          payload: { key: 'bulletin_campus_all', value: data.total },
         });
-      });
-  }, []);
-
-  React.useEffect(() => {
-    fetch('/api/bulletin/statistic?option=tuijian-today')
-      .then((response) => response.json())
-      .then((data) => {
         dispatch({
           type: 'set',
-          payload: { key: 'bulletin_tuijian_today', value: data.qty },
-        });
-      });
-  }, []);
-
-  React.useEffect(() => {
-    fetch('/api/bulletin/statistic?option=tuijian-all')
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch({
-          type: 'set',
-          payload: { key: 'bulletin_tuijian_all', value: data.qty },
+          payload: { key: 'bulletin_campus_today', value: data.today },
         });
       });
   }, []);
@@ -194,11 +198,11 @@ function Home() {
                   <div className="col">
                     <div className="card bg-dark shadow">
                       <div className="card-body">
-                        <p className="lead">简历投递次数 / 岗位</p>
+                        <p className="lead">简历投递次数 今日新增 / 总计</p>
                         <h1 className="display-1 text-center">
-                          {state.candidate_send}
+                          {state.candidate_send_today}
                           <hr />
-                          {state.employer_job}
+                          {state.candidate_send_all}
                         </h1>
                       </div>
                     </div>
@@ -218,8 +222,21 @@ function Home() {
                   </div>
                 </div>
 
-                <div className="row mt-4 d-flex justify-content-center">
+                <div className="row mt-4 d-flex justify-content-between">
                   <div className="col-4">
+                    <div className="card bg-dark shadow">
+                      <div className="card-body">
+                        <p className="lead">岗位 - 今日新增 / 总计</p>
+                        <h1 className="display-1 text-center">
+                          {state.employer_job_today}
+                          <hr />
+                          {state.employer_job_all}
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="col">
                     <div className="card bg-dark shadow">
                       <div className="card-body">
                         <p className="lead">推荐信息 - 今日新增 / 总计</p>
@@ -227,6 +244,19 @@ function Home() {
                           {state.bulletin_tuijian_today}
                           <hr />
                           {state.bulletin_tuijian_all}
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="col">
+                    <div className="card bg-dark shadow">
+                      <div className="card-body">
+                        <p className="lead">校园招聘 - 今日新增 / 总计</p>
+                        <h1 className="display-1 text-center">
+                          {state.bulletin_campus_today}
+                          <hr />
+                          {state.bulletin_campus_all}
                         </h1>
                       </div>
                     </div>
