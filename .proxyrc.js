f22731a002046a70f2889891fdcde375f5e18718
ovaphlow <ovaphlow@live.com>
2022-01-03 14:59:04 +0800

    Update .proxyrc.js

--- .proxyrc.js
+++ .proxyrc.js
@@ -1,14 +1,19 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(
-    createProxyMiddleware('/api/miscellaneous', {
-      target: 'http://localhost:8421/',
-    }),
-  );
+  // app.use(
+  //   createProxyMiddleware('/api/miscellaneous', {
+  //     target: 'http://localhost:8421/',
+  //   }),
+  // );
+  // app.use(
+  //   createProxyMiddleware('/api', {
+  //     target: 'http://localhost:8081/',
+  //   }),
+  // );
   app.use(
     createProxyMiddleware('/api', {
-      target: 'http://localhost:8081/',
+      target: 'http://localhost:8421/',
     }),
   );
 };
